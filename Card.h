10103f1d1f8dd56e0d898435c2efe5dd3aafba86
sanshous <63168235+sanshous@users.noreply.github.com>
2020-05-13 01:12:13 +0300

    Add files via upload

--- Card.h
+++ Card.h
@@ -5,20 +5,24 @@
 class Card
 {
 public:
-	Card(string name, int score, void func(), int t)
+	Card(string name, int score, void(*func)(), int t)
 	{
 		name_card = name;
 		power_score = score;
 		type = t;
 		min_act = func;
+		next = nullptr;
+	}
+	void add_list(class Card* q)
+	{
+		next = q;
 	}
 	void (*min_act)();
 private:
+	class Card* next;
 	string name_card;
 	int power_score;
 	int type; // 0 - приспешник, 1 - действие
-	class Card* next;
-	class Card* prev;
 };
 
 class Fraction
--- fractions.cpp
+++ fractions.cpp
@@ -6,9 +6,102 @@ using namespace std;
 #include "Card.h"
 #include "Base.h"
 
-Card* pirate1 = new Card("Pirate King", 5, special1());
-Card* pirate2 = new Card("Buccaneer", 4, special2());
-Card* pirate3 = new Card("Saucy Wench", 3, special3());
-Card* pirate4 = new Card("First Mate", 2, special4());
-Card* action1 = new Card("Dinghy", act1());
-Card* action2 = new Card("Broadside", act2());
+void special1();
+void special2();
+void special3();
+void special4();
+void act1();
+void act2();
+void act3();
+void act4();
+void act5();
+void act6();
+void act7();
+void act8();
+
+Card* pirate1 = new Card("Pirate King", 5, special1, 0);
+Card* pirate2 = new Card("Buccaneer", 4, special2, 0);
+Card* pirate3 = new Card("Saucy Wench", 3, special3, 0);
+Card* pirate4 = new Card("First Mate", 2, special4, 0);
+Card* action1 = new Card("Dinghy", 0, act1, 1);
+Card* action2 = new Card("Broadside", 0, act2, 1);
+Card* action3 = new Card("Shanghai", 0, act3, 1);
+Card* action4 = new Card("Powderkeg", 0, act4, 1);
+Card* action5 = new Card("Swashbuckling", 0, act5, 1);
+Card* action6 = new Card("Cannon", 0, act6, 1);
+Card* action7 = new Card("Full Sail", 0, act7, 1);
+Card* action8 = new Card("Sea Dogs", 0, act8, 1);
+
+Card* dino1 = new Card("King Rex", 7, special1, 0);
+Card* dino2 = new Card("Laseratops", 4, special2, 0);
+Card* dino3 = new Card("Armor Stego", 3, special3, 0);
+Card* dino4 = new Card("War Raptor", 2, special4, 0);
+Card* actio1 = new Card("Augmentation", 0, act1, 1);
+Card* actio2 = new Card("Howl", 0, act2, 1);
+Card* actio3 = new Card("Natural Selection", 0, act3, 1);
+Card* actio4 = new Card("Upgrade", 0, act4, 1);
+Card* actio5 = new Card("Tooth and Claw... and Guns", 0, act5, 1);
+Card* actio6 = new Card("Survival of the Fittest", 0, act6, 1);
+Card* actio7 = new Card("Wildlife Preserve", 0, act7, 1);
+Card* actio8 = new Card("Rampage", 0, act8, 1);
+
+Card* prank1 = new Card("Leprechaun", 5, special1, 0);
+Card* prank2 = new Card("Brownie", 4, special2, 0);
+Card* prank3 = new Card("Gnome", 3, special3, 0);
+Card* prank4 = new Card("Gremlin", 2, special4, 0);
+Card* acti1 = new Card("Disenchant", 0, act1, 1);
+Card* acti2 = new Card("Enshrouding Mist", 0, act2, 1);
+Card* acti3 = new Card("Pay the Piper", 0, act3, 1);
+Card* acti4 = new Card("Mark of Sleep", 0, act4, 1);
+Card* acti5 = new Card("Block the Path", 0, act5, 1);
+Card* acti6 = new Card("Take the Shinies", 0, act6, 1);
+Card* acti7 = new Card("Flame Trap", 0, act7, 1);
+Card* acti8 = new Card("Hideout", 0, act8, 1);
+
+void special1()
+{
+}
+
+void special2()
+{
+}
+
+void special3()
+{
+}
+
+void special4()
+{
+}
+
+void act1()
+{
+}
+
+void act2()
+{
+}
+
+void act3()
+{
+}
+
+void act4()
+{
+}
+
+void act5()
+{
+}
+
+void act6()
+{
+}
+
+void act7()
+{
+}
+
+void act8()
+{
+}
